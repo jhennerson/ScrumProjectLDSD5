3c91bc7e9b8bb1865bd46995cc9d9679ef08d9a3
Augusto Souza <Augustosouzas01@gmail.com>
2023-09-16 17:38:06 -0300

    solving merge conflicts with guilherme

--- scrum-project/scrum-spring/src/main/java/com/ifsp/scrumProjectLDSD5/model/User.java
+++ scrum-project/scrum-spring/src/main/java/com/ifsp/scrumProjectLDSD5/model/User.java
@@ -1,44 +1,30 @@
-package com.ifsp.scrumProjectLDSD5.model;
+package com.ifsp.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
+@Table(name="USER")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(columnDefinition = "BIGINT")
+    private Long id;
 
-	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	private String username;
-	private String password;
-	private String email;
-	
-	
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	
-	
+    @Column(name = "username", nullable = false)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    public static Object builder() {
+
+        return null;
+    }
 }
