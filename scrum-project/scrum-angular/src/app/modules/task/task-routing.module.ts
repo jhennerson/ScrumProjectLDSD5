bf18e4fb926b799f50c42fbe72b67b0b518b4697
Jhennerson Barbosa da Silva <92925449+jhennerson@users.noreply.github.com>
2023-11-12 17:40:13 -0300

    Layout refactored on pages with menu

--- scrum-project/scrum-angular/src/app/modules/task/task-routing.module.ts
+++ scrum-project/scrum-angular/src/app/modules/task/task-routing.module.ts
@@ -1,14 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TaskComponent } from 'src/app/components/task/task.component';
-import { BoardComponent } from 'src/app/components/board/board.component';
-import { UserStoryComponent } from 'src/app/components/user-story/user-story.component';
 
-const routes: Routes = [
-  { path: '', component: TaskComponent },
-  { path: 'board', component: BoardComponent },
-  { path: 'user-story', component: UserStoryComponent },
-];
+const routes: Routes = [{ path: '', component: TaskComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- scrum-project/scrum-angular/src/app/modules/waste-bin/waste-bin-routing.module.ts
+++ scrum-project/scrum-angular/src/app/modules/waste-bin/waste-bin-routing.module.ts
@@ -1,16 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { TaskComponent } from 'src/app/components/task/task.component';
-import { BoardComponent } from 'src/app/components/board/board.component';
-import { UserStoryComponent } from 'src/app/components/user-story/user-story.component';
 import { WasteBinComponent } from 'src/app/components/waste-bin/waste-bin.component';
 
-const routes: Routes = [
-  { path: '', component: WasteBinComponent },
-  { path: 'board', component: BoardComponent },
-  { path: 'task', component: TaskComponent },
-  { path: 'user-story', component: UserStoryComponent },
-];
+const routes: Routes = [{ path: '', component: WasteBinComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
